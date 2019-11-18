4b4b719e37edd8dea35394eeba7e9d87b52eae86
JacksonJ01 <jacksonj@hartwick.edu>
2019-11-18 16:17:57 -0500

    Pretty straight forward

--- Ex.40.py
+++ Ex.40.py
@@ -1,1 +1,25 @@
+# Jackson J.
+# 11/18/19
+# Exercise 40 and it is continuing the lesson about creating a class
 
+
+class Song(object):
+    # This is the class creation "Song"
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+
+happy_bday = Song(["Happy birthday to you",
+                   "I don't want to get sued",
+                   "So I'll stop right there"])
+
+bulls_on_parade_song = Song(["They rally around tha family",
+                             "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade_song.sing_me_a_song()
